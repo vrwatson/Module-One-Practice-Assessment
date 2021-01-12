1ea0d938a20d9cacd971184c24ecb92575b69a58
Corey Ladovsky <coreyskymusic@gmail.com>
2021-01-12 12:22:44 -0800

    all questions written

---
+++ problems/everyOtherLetter.js
@@ -0,0 +1,12 @@
+/**
+ * Returns a new string with every other letter.
+ * @param {string} str - a string
+ * @returns {str} - 
+ *
+ * ex: everyOtherLetter("mississippi") //=> "msispi"
+ *
+ */
+
+function everyOtherLetter() {}
+
+module.exports = everyOtherLetter;
---
+++ problems/howManyTargets.js
@@ -0,0 +1,13 @@
+/**
+ * Returns the number of times the target is in the array
+ * @param {Array} arr - array of elements
+ * @param {any} target - target to find in array
+ * @returns {number} - amount of times the target was included
+ *
+ * ex: howManyTargets([1, 2, 3, 2, 1, 1], 1) //=> 3
+ *
+ */
+
+function howManyTargets() {}
+
+module.exports = howManyTargets;
